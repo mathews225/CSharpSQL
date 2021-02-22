ffedaf5d935e23dd1d953cbf50b8ce6c018fa391
mathews225 <petemathews14@gmail.com>
2021-02-22 16:14:39 -0500

    Add project files.

---
+++ CSharpSqlConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CSharpSQL;
+
+namespace CSharpSqlConsole {
+	class Program {
+		static void Main(string[] args) {
+
+			var sql = new EduDbLib();
+			sql.Connect("EdDb");
+			Console.WriteLine("Con...\n   nect..\n\t.ion is..\n\t\t weak.\n\t\t\t...J/k the connection is fine.");
+			sql.Disconnect();
+
+		}
+	}
+}
---
+++ TestCSharpSQL/EduDbLib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace CSharpSQL {
+	public class EduDbLib {
+
+		public SqlConnection connection { get; set; }
+
+		public void Connect(string database) {
+			var connStr = $"server=localhost\\sqlexpress;" +
+				$"database={database};" +
+				$"trusted_connection=true;";
+			connection = new SqlConnection(connStr);
+			connection.Open();
+			if(connection.State != System.Data.ConnectionState.Open) {
+				throw new Exception("Connection is not open! ...Or is it!? ...It isn't.");
+			}
+		}
+
+
+		public void Disconnect() {
+			connection.Close();
+		}
+
+
+
+
+	}
+}
